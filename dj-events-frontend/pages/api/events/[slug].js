8fe4c964c944933d8f827f555dfc609174dba499
TextError <savinraul1983@yahoo.com>
2021-06-01 19:03:16 +0100

    Slug component - getStaticProps

--- dj-events-frontend/pages/api/events/[slug].js
+++ dj-events-frontend/pages/api/events/[slug].js
@@ -4,7 +4,7 @@ export default (req, res) => {
   const { slug } = req.query;
   const evt = events.filter(el => el.slug === slug);
 
-  if(req.method === 'GET') return res.status(200).json(evt);
+  if(req.method === 'GET') return res.status(200).json(...evt);
   res.setHeader('Allow', ['GET']);
   return res.status(405).json({ message: `${req.method} is not allowed` });
 };
--- dj-events-frontend/pages/events/[slug].jsx
+++ dj-events-frontend/pages/events/[slug].jsx
@@ -1,9 +1,37 @@
+import { API_URL } from "@/config/index";
 import Layout from "@/layout/Layout";
 
-const SlugPage = () => (
+const SlugPage = ({  }) => (
   <Layout>
     SlugPage
   </Layout>
 )
 
-export default SlugPage
+export default SlugPage;
+
+// export const getServerSideProps = async ({ query: { slug }}) => {
+//   const evt = await (await fetch(`${API_URL}/api/events/${slug}`)).json();
+
+//   return {
+//     props: { ...evt }
+//   }
+// };
+
+export const getStaticPaths = async () => {
+  const events = await (await fetch(`${API_URL}/api/events`)).json();
+  const paths = events.map(evt => ({ params: { slug: evt.slug } }));
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps = async ({ params: { slug }}) => {
+  const evt = await (await fetch(`${API_URL}/api/events/${slug}`)).json();
+
+  return {
+    props: { ...evt },
+    revalidate: 1
+  }
+};
