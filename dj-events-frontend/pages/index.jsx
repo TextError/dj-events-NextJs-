1cf6b39e53dbc67e1a527fbd3d456a22a2cb353b
TextError <savinraul1983@yahoo.com>
2021-06-01 18:18:48 +0100

    EventItem component

--- dj-events-frontend/pages/index.jsx
+++ dj-events-frontend/pages/index.jsx
@@ -1,10 +1,12 @@
 import Layout from "@/layout/Layout";
 import { API_URL } from "@/config/index";
+import EventItem from "components/home/EventItem";
 
 const Home = ({ events }) => (
   <Layout>
     <h1>Upcoming Events</h1>
-    {console.log(events)}
+    {events.length === 0 && <h2>No svents to show.</h2>}
+    {events.map((evt, i) => <EventItem key={i} evt={evt} />)}
   </Layout>
 );
 
